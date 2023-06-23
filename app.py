538cad478d339d3fb66b48b764c61b48230bdd74
Mayuresh Nehe <mayureshnehe@gmail.com>
2023-06-23 14:23:33 +0530

    dw

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ openai.api_key = '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'
 app = Flask(__name__)
 
 # Function to fetch user repositories from GitHub API
+
+
+# Function to fetch user repositories from GitHub API
 def fetch_user_repos(github_url):
     username = extract_username(github_url)
     api_url = f'https://api.github.com/users/{username}/repos'
@@ -23,7 +26,6 @@ def fetch_user_repos(github_url):
     else:
         print(f"Failed to fetch repositories. Error: {response.text}")
         return []
-    # Same code as before
 
 # Helper function to extract username from GitHub URL
 def extract_username(github_url):
@@ -33,41 +35,42 @@ def extract_username(github_url):
         return match.group(1)
     else:
         raise ValueError("Invalid GitHub URL format")
-    # Same code as before
 
 # Function to clone a repository using GitPython
 def clone_repository(repo_url, destination):
     git.Repo.clone_from(repo_url, destination)
-    # Same code as before
 
 # Function to preprocess code before passing it to GPT
 def preprocess_code(code):
+    # Implement your preprocessing techniques here
+    # such as memory management, token limits, etc.
     processed_code = code  # Placeholder, modify as needed
     return processed_code
-    # Same code as before
 
 # Function to assess technical complexity using GPT
 def assess_complexity(code):
+    # Implement your prompt engineering techniques here
+    # to evaluate the technical complexity of the code using GPT
     complexity_score = 0.5  # Placeholder, modify as needed
     return complexity_score
-    # Same code as before
 
 # Function to remove a directory and its contents
 def remove_directory(directory):
     shutil.rmtree(directory)
-    # Same code as before
 
 # Placeholder function to get repository files
 def get_repository_files(repository_path):
+    # Implement your logic to retrieve files from the cloned repository
+    # and return them for further processing
     files = []  # Placeholder, modify as needed
     return files
-    # Same code as before
 
 # Placeholder function to combine files content
 def combine_files_content(files):
+    # Implement your logic to read the content of the files
+    # and combine them into a single code snippet or string
     combined_content = ''  # Placeholder, modify as needed
     return combined_content
-    # Same code as before
 
 # Main function to find the most complex repository
 def find_most_complex_repository(github_url):
@@ -104,15 +107,13 @@ def find_most_complex_repository(github_url):
             # Clean up cloned repository
             if os.path.exists(clone_path):
                 remove_directory(clone_path)
-                
+
     if most_complex_repo:
         print(f"The most complex repository is: {most_complex_repo['name']}")
         print(f"Complexity score: {highest_complexity_score}")
         print(f"Repository URL: {most_complex_repo['html_url']}")
     else:
         print("No complex repository found.")
-
-    # Same code as before
 
 # Flask route for the homepage
 @app.route('/', methods=['GET', 'POST'])
