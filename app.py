394b6e2500811b69f7bb461c427aa87f23a68dbf
Mayuresh Nehe <mayureshnehe@gmail.com>
2023-06-24 11:44:59 +0530

    new

--- app.py
+++ app.py
@@ -1,130 +1,87 @@
 from flask import Flask, render_template, request
-import os
-import re
-import shutil
 import requests
-import git
-import openai
+import os
+import subprocess
 
-# Set up OpenAI API credentials
-openai.api_key = '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'
-
-# Set up Flask app
 app = Flask(__name__)
 
-# Function to fetch user repositories from GitHub API
-
-
-# Function to fetch user repositories from GitHub API
-def fetch_user_repos(github_url):
-    username = extract_username(github_url)
-    api_url = f'https://api.github.com/users/{username}/repos'
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        repos = response.json()
-        return repos
-    else:
-        print(f"Failed to fetch repositories. Error: {response.text}")
-        return []
-
-# Helper function to extract username from GitHub URL
-def extract_username(github_url):
-    pattern = r'https://github.com/([^/]*)'
-    match = re.match(pattern, github_url)
-    if match:
-        return match.group(1)
-    else:
-        raise ValueError("Invalid GitHub URL format")
-
-# Function to clone a repository using GitPython
-def clone_repository(repo_url, destination):
-    git.Repo.clone_from(repo_url, destination)
-
-# Function to preprocess code before passing it to GPT
-def preprocess_code(code):
-    # Implement your preprocessing techniques here
-    # such as memory management, token limits, etc.
-    processed_code = code  # Placeholder, modify as needed
-    return processed_code
-
-# Function to assess technical complexity using GPT
-def assess_complexity(code):
-    # Implement your prompt engineering techniques here
-    # to evaluate the technical complexity of the code using GPT
-    complexity_score = 0.5  # Placeholder, modify as needed
-    return complexity_score
-
-# Function to remove a directory and its contents
-def remove_directory(directory):
-    shutil.rmtree(directory)
-
-# Placeholder function to get repository files
-def get_repository_files(repository_path):
-    # Implement your logic to retrieve files from the cloned repository
-    # and return them for further processing
-    files = []  # Placeholder, modify as needed
-    return files
-
-# Placeholder function to combine files content
-def combine_files_content(files):
-    # Implement your logic to read the content of the files
-    # and combine them into a single code snippet or string
-    combined_content = ''  # Placeholder, modify as needed
-    return combined_content
-
-# Main function to find the most complex repository
-def find_most_complex_repository(github_url):
-    repos = fetch_user_repos(github_url)
-
-    if not repos:
-        print("No repositories found.")
-        return
-
-    most_complex_repo = None
-    highest_complexity_score = 0
-
-    for repo in repos:
-        repo_url = repo['html_url']
-        repo_name = repo['name']
-        clone_path = f"temp/{repo_name}"
-
-        try:
-            clone_repository(repo_url, clone_path)
-            files = get_repository_files(clone_path)
-            code = combine_files_content(files)
-            preprocessed_code = preprocess_code(code)
-            complexity_score = assess_complexity(preprocessed_code)
-
-            if complexity_score > highest_complexity_score:
-                highest_complexity_score = complexity_score
-                most_complex_repo = repo
-
-        except Exception as e:
-            print(f"Error processing repository: {repo_name}")
-            print(f"Error details: {str(e)}")
-
-        finally:
-            # Clean up cloned repository
-            if os.path.exists(clone_path):
-                remove_directory(clone_path)
-
-    if most_complex_repo:
-        print(f"The most complex repository is: {most_complex_repo['name']}")
-        print(f"Complexity score: {highest_complexity_score}")
-        print(f"Repository URL: {most_complex_repo['html_url']}")
-    else:
-        print("No complex repository found.")
-
-# Flask route for the homepage
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        github_url = request.form['github-url']
-        result = find_most_complex_repository(github_url)
-        return render_template('index.html', result=result)
-    else:
-        return render_template('index.html', result=None)
+        github_url = request.form['github_url']
+        repositories = fetch_user_repositories(github_url)
 
-# Run the Flask app
+        if repositories:
+            most_complex_repository = find_most_complex_repository(repositories)
+            complexity_justification = gpt_justification(most_complex_repository)
+            analysis = perform_gpt_analysis(most_complex_repository)  # Perform GPT-based analysis
+            return render_template('result.html', repo_url=most_complex_repository["html_url"], analysis=analysis)
+        else:
+            return render_template('index.html', result=False)
+    return render_template('index.html', result=None)
+
+
+
+def fetch_user_repositories(github_url):
+    # Extract username from GitHub URL
+    username = github_url.split("/")[-1]
+
+    # Send request to GitHub API to fetch user repositories
+    api_url = f"https://api.github.com/users/{username}/repos"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        repositories = response.json()
+        return repositories
+    else:
+        print("Failed to fetch user repositories.")
+        return []
+
+def preprocess_code(repository):
+    # Clone the repository locally
+    subprocess.call(["git", "clone", repository["clone_url"]])
+
+    # Perform memory management and preprocessing on the code files
+    repo_name = repository["name"]
+    repo_path = os.path.join(os.getcwd(), repo_name)
+
+    # Add your memory management and preprocessing techniques here
+
+    # Example: Print the names of all Python files in the repository
+    for root, dirs, files in os.walk(repo_path):
+        for file in files:
+            if file.endswith(".py"):
+                print(os.path.join(root, file))
+
+def evaluate_technical_complexity(code):
+    # Implement your prompt engineering techniques and GPT evaluation here
+    # Example: Return a random complexity score for demonstration purposes
+    return 0.75
+
+def find_most_complex_repository(repositories):
+    most_complex_repo = None
+    highest_complexity_score = 0
+
+    for repository in repositories:
+        preprocess_code(repository)
+        complexity_score = evaluate_technical_complexity(repository["name"])
+
+        if complexity_score > highest_complexity_score:
+            highest_complexity_score = complexity_score
+            most_complex_repo = repository
+
+    return most_complex_repo
+
+def gpt_justification(repository):
+    # Implement GPT-based justification for the selection of the repository here
+    # Example: Return a placeholder justification string for demonstration purposes
+    return "The selected repository contains advanced algorithms and extensive usage of machine learning techniques, making it highly complex."
+
+def perform_gpt_analysis(repository):
+    # Implement GPT-based analysis logic here
+    # Example: Return a placeholder analysis string for demonstration purposes
+    return "The selected repository demonstrates a high level of technical complexity with intricate algorithm design and extensive use of advanced data structures."
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
